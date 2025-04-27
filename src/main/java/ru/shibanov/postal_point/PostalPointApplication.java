9ea8ef3ff64839202d280041c507108e1bc6d28e
German <gnshibanov@mail.ru>
2025-04-27 10:28:11 +0300

    first commit

---
+++ src/main/java/ru/shibanov/postal_point/PostalPointApplication.java
@@ -0,0 +1,13 @@
+package ru.shibanov.postal_point;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostalPointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostalPointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/shibanov/postal_point/PostalPointApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.shibanov.postal_point;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostalPointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
