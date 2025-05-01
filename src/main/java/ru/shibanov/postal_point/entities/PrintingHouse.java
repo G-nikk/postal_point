39cb32837f83db1636a75acb4217651530eca1d1
German <gnshibanov@mail.ru>
2025-05-01 14:40:43 +0300

    Created PrintingHouse.java

---
+++ src/main/java/ru/shibanov/postal_point/entities/PrintingHouse.java
@@ -0,0 +1,63 @@
+package ru.shibanov.postal_point.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "PrintingHouse")
+public class PrintingHouse {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "PrintingHouseID")
+    private Integer printingHouseID;
+
+    @Column(name = "Name", nullable = false)
+    private String name;
+
+    @Column(name = "Address")
+    private String address;
+
+    @OneToMany(mappedBy = "printingHouse")
+    private List<PrintRun> printRuns;
+
+    public Integer getPrintingHouseID() {
+        return printingHouseID;
+    }
+
+    public void setPrintingHouseID(Integer printingHouseID) {
+        this.printingHouseID = printingHouseID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public List<PrintRun> getPrintRuns() {
+        return printRuns;
+    }
+
+    public void setPrintRuns(List<PrintRun> printRuns) {
+        this.printRuns = printRuns;
+    }
+
+    @Override
+    public String toString() {
+        return "PrintingHouse{" +
+                "printingHouseID=" + printingHouseID +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
