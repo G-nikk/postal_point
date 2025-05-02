8550bd28ccb1e7efa1b772222325ddb321495453
German <gnshibanov@mail.ru>
2025-05-02 14:01:44 +0300

    Created PrintingHouseService.java

---
+++ src/main/java/ru/shibanov/postal_point/services/PrintingHouseService.java
@@ -0,0 +1,23 @@
+package ru.shibanov.postal_point.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.shibanov.postal_point.entities.PrintingHouse;
+import ru.shibanov.postal_point.repositories.PrintingHouseRepository;
+
+@Service
+@Transactional
+public class PrintingHouseService {
+    private final PrintingHouseRepository printingHouseRepository;
+
+    @Autowired
+    public PrintingHouseService(PrintingHouseRepository printingHouseRepository) {
+        this.printingHouseRepository = printingHouseRepository;
+    }
+
+    @Transactional
+    public void save(PrintingHouse printingHouse) {
+        printingHouseRepository.save(printingHouse);
+    }
+}
