a4bfceeadeea67910e2a7d1c0022e40fa513ff49
German <gnshibanov@mail.ru>
2025-05-23 18:36:46 +0300

    Added PostOffices page routing

--- newspaper-frontend/src/App.js
+++ newspaper-frontend/src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route, Link } from 'react-router-dom';
 import NewspaperList from './components/NewspaperList';
 import PrintingHouseList from './components/PrintingHouseList';
+import PostOfficeList from './components/PostOfficeList';
 
 export default function App() {
     return (
@@ -9,6 +10,7 @@ export default function App() {
                 <Link to="/newspapers" style={{ textDecoration: 'none', color: '#1976d2' }}>
                     Управление газетами
                 </Link>
+                <Link to="/post-offices" style={{ textDecoration: 'none', color: '#1976d2' }}>Почтовые отделения</Link>
                 <Link to="/printing-houses" style={{ textDecoration: 'none', color: '#1976d2' }}>Управление типографиями</Link>
             </nav>
 
@@ -16,6 +18,7 @@ export default function App() {
                 <Route path="/newspapers" element={<NewspaperList />} />
                 <Route path="/printing-houses" element={<PrintingHouseList />} />
                 <Route path="/" element={<h1 style={{ textAlign: 'center' }}>Добро пожаловать в систему управления газетами</h1>} />
+                <Route path="/post-offices" element={<PostOfficeList />} />
             </Routes>
         </div>
     );
